cec4ca05b1828b7e75c91d33b06695ba3c2f0f93
Ryan <apo.ryandale@gmail.com>
2020-09-15 15:37:14 +0800

    Initial Commit

---
+++ YelpAssignment/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  YelpAssignment
+//
+//  Created by Ryan Dale Apo on 9/15/20.
+//  Copyright © 2020 Ryan Dale Apo. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
